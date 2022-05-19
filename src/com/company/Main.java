c462c042d8070823adea8a6282b219efea3bc7b8
datus1 <104738487+datus1@users.noreply.github.com>
2022-05-19 17:42:27 +0300

    Labwork 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,22 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        double gravity = -9.81; // Earth's gravity in m/s^2
-        double initialVelocity = 0.0;
-        double fallingTime = 10.0;
-        double initialPosition = 0.0;
-        double finalPosition = 0.0;
-        System.out.println("The object's position after " + fallingTime +
-                " seconds is " + finalPosition + " m.");
-    }
-}  
-
-
-package com.company;
-
-public class Main {
-
-    public static void main(String[] args) {
         double a = -9.81; // a represents Earth's gravity in m/s^2
         double vi = 0.0; // vi represents initial velocity
         double t = 10.0; // t represents falling time
